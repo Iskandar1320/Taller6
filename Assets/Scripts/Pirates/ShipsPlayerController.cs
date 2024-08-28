1348bcde0956b8a717a2ebf1dbf1e2fea25278a9
jagari5546 <122942343+jagari5546@users.noreply.github.com>
2024-08-28 01:33:50 -0500

    Giro del Barco y Volante Anadidos, Movimiento Automatico del Barco

---
+++ Assets/Scripts/Pirates/ShipsPlayerController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ShipsPlayerController : MonoBehaviour
+{
+   
+    [Header("Movement Settings")]
+    [SerializeField] Vector2 speed;
+    [SerializeField] int playerNumber;
+    [SerializeField] float rotationSpeed = 90f;
+    [SerializeField] AudioClip shooting_Audio;
+    [SerializeField] AudioClip movement_Audio;
+    [SerializeField] SteeringWheel steeringWheel;
+
+    private Rigidbody2D rb;
+    private float steeringInput;
+    
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+   
+
+
+    private void FixedUpdate()
+    {
+        steeringInput = steeringWheel.GetClampedValue();
+
+        float rotationAmount = steeringInput   * rotationSpeed *Time.deltaTime;
+        rb.rotation += rotationAmount;
+
+        Vector2 forwardMovement = transform.up * speed.y * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + forwardMovement);
+    }
+    public void OnShot()
+    {
+        Debug.Log("Mathafakas");
+    }
+
+}
