ada960bf1ba82cab8e6283b0c3487c031c88ec79
jagari5546 <122942343+jagari5546@users.noreply.github.com>
2024-08-28 00:29:32 -0500

    Correcciones

--- Assets/Scripts/ShipsPlayerController.cs
+++ Assets/Scripts/ShipsPlayerController.cs
@@ -1,7 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.Events;
 
 public class ShipsPlayerController : MonoBehaviour
 {
@@ -16,8 +17,20 @@ public class ShipsPlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GetBodyMovement();
+        GetShootingInput();
     }
+
+    private void GetShootingInput()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void GetBodyMovement()
+    {
+        throw new NotImplementedException();
+    }
+
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + Speed * Time.fixedDeltaTime);
