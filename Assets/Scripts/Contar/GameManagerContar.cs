6c31a58f08cd322677b8c672d2bf3aee8820c27e
jesusjurado2000 <80048009+jesusjurado2000@users.noreply.github.com>
2024-08-29 19:29:18 -0500

    Contar

---
+++ Assets/Scripts/Contar/GameManagerContar.cs
@@ -0,0 +1,45 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerContar : MonoBehaviour
+{
+    public GameObject PjroAma;
+    public GameObject PjroVer;
+
+    public float maxX;
+    public Transform spawnPoint;
+    public float spawnRate;
+
+    bool gameStarted = false;
+    
+   
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && !gameStarted)
+        {
+            StartSpawning();    
+            gameStarted = true;
+        }
+    }
+
+    private void StartSpawning()
+    {
+        InvokeRepeating("SpawnPjroAma", 0.2f, spawnRate);
+        InvokeRepeating("SpawnPjroVer", 0.5f, spawnRate);
+    }
+    void SpawnPjroAma()
+    {
+        Vector3 spawnPos = spawnPoint.position;
+        spawnPos.x = Random.Range(-maxX, maxX);
+        Instantiate(PjroAma, spawnPos, Quaternion.identity);
+    }
+    void SpawnPjroVer()
+    {
+        Vector3 spawnPos = spawnPoint.position;
+        spawnPos.x = Random.Range(-maxX, maxX);
+        Instantiate(PjroVer, spawnPos, Quaternion.identity);
+    }
+
+}
