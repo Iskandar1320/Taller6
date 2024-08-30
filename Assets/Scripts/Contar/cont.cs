ca99b3848378d03818a06747f9ff50767b7d85ac
jesusjurado2000 <80048009+jesusjurado2000@users.noreply.github.com>
2024-08-30 01:08:49 -0500

    contar4

--- Assets/Scripts/Contar/cont.cs
+++ Assets/Scripts/Contar/cont.cs
@@ -1,10 +1,31 @@
 using UnityEngine;
+using System.Collections;
 
 public class ContadorDeColisiones : MonoBehaviour
 {
+    GameManagerContar gameManager;
+    private int diferencia1;
+    private int diferencia2;
     public int contador = 0;
     public int contAzul = 0;
     public int contRojo = 0;
+    private float tiempoDeEspera = 35f;
+    public GameObject AzulGana;
+    public GameObject RojoGana;
+    public GameObject Empate;
+
+
+    private void Start()
+    {
+        gameManager = GameObject.FindObjectOfType<GameManagerContar>();
+    }
+    private void Update()
+    {
+        if(gameManager.gameStarted == true)
+        {
+            StartCoroutine(EjecutarAccionDespuesDeTiempo());
+        }
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("PjroAma"))
@@ -24,20 +45,29 @@ public class ContadorDeColisiones : MonoBehaviour
     }
     private void CompararContadores()
     {
-        int diferencia1 = Mathf.Abs(contador - contAzul);
-        int diferencia2 = Mathf.Abs(contador - contRojo);
+        diferencia1 = Mathf.Abs(contador - contAzul);
+        diferencia2 = Mathf.Abs(contador - contRojo);
+    }
+
+
+    private IEnumerator EjecutarAccionDespuesDeTiempo()
+    {
+        yield return new WaitForSeconds(tiempoDeEspera);    
 
         if (diferencia1 < diferencia2)
         {
             Debug.Log("Azul gana");
+            AzulGana.SetActive(true);
         }
         else if (diferencia2 < diferencia1)
         {
             Debug.Log("Rojo gana");
+            RojoGana.SetActive(true);
         }
         else
         {
             Debug.Log("Empate");
+            Empate.SetActive(true);
         }
     }
 }
