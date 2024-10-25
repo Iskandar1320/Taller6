42d7dd555e72b51e6d68add785367855ec038d3f
jesusjurado2000 <80048009+jesusjurado2000@users.noreply.github.com>
2024-10-25 12:06:15 -0500

    atrapar

--- Assets/Scripts/Atrapar/Puntos.cs
+++ Assets/Scripts/Atrapar/Puntos.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class Puntos : MonoBehaviour
 {
     public float speed = 5f;
-
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
@@ -14,22 +13,12 @@ public class Puntos : MonoBehaviour
     }
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Colisión detectada con: " + other.name);
         if (other.CompareTag("TrianAzul"))
         {
-            Debug.Log("Colisión con el jugador azul");
-            Destroy(gameObject);
+            Destroy(this.gameObject);
         }
         if (other.CompareTag("TrianRojo"))
         {
-            Destroy(gameObject);
-        }
-    }
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.collider.CompareTag("TrianAzul"))
-        {
-            Debug.Log("Colisión con el jugador detectada");
             Destroy(gameObject);
         }
     }
