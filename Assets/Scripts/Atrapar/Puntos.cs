ffd9155635f9b99386a09afd8e0aee267e3e2463
jesusjurado2000 <80048009+jesusjurado2000@users.noreply.github.com>
2024-10-29 00:37:04 -0500

    CoreAtrapar

--- Assets/Scripts/Atrapar/Puntos.cs
+++ Assets/Scripts/Atrapar/Puntos.cs
@@ -1,26 +1,41 @@
 using UnityEngine;
 
-public class Puntos : MonoBehaviour
+public class CircleMovement : MonoBehaviour
 {
     public float speed = 5f;
+    public GameObject particleEffect;
+    public GameObject circle;
+    string AzulTag = "TrianAzul";
+    string RojoTag = "TrianRojo";
+    private bool shouldMove = true;
+
     void Update()
     {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        if (shouldMove)
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
         if (transform.position.x > Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0)).x)
         {
             Destroy(gameObject);
         }
     }
-    void OnTriggerEnter2D(Collider2D other)
+
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        if (other.CompareTag("TrianAzul"))
+        if (collision.CompareTag(AzulTag))
         {
-            Destroy(this.gameObject);
+            particleEffect.SetActive(true);
+            shouldMove = false;
+            Destroy(circle);
+            Destroy(gameObject, 2f);
         }
-        if (other.CompareTag("TrianRojo"))
+        if (collision.CompareTag(RojoTag))
         {
-            Destroy(gameObject);
+            particleEffect.SetActive(true);
+            shouldMove = false;
+            Destroy(circle);
+            Destroy(gameObject, 2f);
         }
     }
-
 }
