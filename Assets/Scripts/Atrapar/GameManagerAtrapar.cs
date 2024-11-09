0fae4d1ecc98302fd0636b1a33449749e0f2f2d8
jesusjurado2000 <80048009+jesusjurado2000@users.noreply.github.com>
2024-11-08 20:15:36 -0500

    AtraparCoreCompleto

--- Assets/Scripts/Atrapar/GameManagerAtrapar.cs
+++ Assets/Scripts/Atrapar/GameManagerAtrapar.cs
@@ -9,15 +9,27 @@ public class GameManagerAtrapar : MonoBehaviour
     public TextMeshProUGUI scoreRojo;
     private int PuntosAzul = 0;
     private int PuntosRojo = 0;
+    public GameObject GanaRojo;
+    public GameObject GanaAzul;
     public void PuntoRojo()
     {
         PuntosRojo++;
         UpdateScoreText();
+        if(PuntosRojo == 25)
+        {
+            Time.timeScale = 0f;
+            GanaRojo.SetActive(true);
+        }
     }
     public void PuntoAzul()
     {
         PuntosAzul++;
         UpdateScoreText();
+        if (PuntosAzul == 25)
+        {
+            Time.timeScale = 0f;
+            GanaAzul.SetActive(true);
+        }
     }
     private void UpdateScoreText()
     {
